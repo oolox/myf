9dca3cb5b644bc28b1b4e9ec5c62d2879d8b435a
Paul Cousineau <oolotronic@Pauls-MacBook-Pro.local>
2017-12-03 18:23:09 -0800

    pre alpha deply

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,3 +4,6 @@ $('#home').load('../frames/myf_home.html');
 $('#menu1').load('../frames/myf_turnbuckles.html');
 $('#menu2').load('../frames/myf_pondBoatTurnbuckles.html');
 $('#menu3').load('../frames/myf_pondBoatFittings.html');
+$('#menu4').load('../frames/myf_goosenecks.html');
+$('#menu5').load('../frames/myf_fittings.html');
+$('#menu6').load('../frames/myf_custom.html');
