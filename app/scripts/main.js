42ef579f251336ead67aeb169149e241baf0d78c
Paul Cousineau <oolotronic@Pauls-MacBook-Pro.local>
2017-12-03 17:30:56 -0800

    tabs 0-3 and imgs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,3 +2,5 @@ console.log('\'Allo \'Allo!');
 
 $('#home').load('../frames/myf_home.html');
 $('#menu1').load('../frames/myf_turnbuckles.html');
+$('#menu2').load('../frames/myf_pondBoatTurnbuckles.html');
+$('#menu3').load('../frames/myf_pondBoatFittings.html');
