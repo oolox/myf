be9f9a6c08e931d91ce37635866d7e73ebed5e35
Paul Cousineau <oolotronic@Pauls-MacBook-Pro.local>
2017-12-21 18:59:36 -0800

    front page update, bigger text, centered image

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,9 @@
 console.log('\'Allo \'Allo!');
 
-$('#home').load('../frames/myf_home.html');
-$('#menu1').load('../frames/myf_turnbuckles.html');
-$('#menu2').load('../frames/myf_pondBoatTurnbuckles.html');
-$('#menu3').load('../frames/myf_pondBoatFittings.html');
-$('#menu4').load('../frames/myf_goosenecks.html');
-$('#menu5').load('../frames/myf_fittings.html');
-$('#menu6').load('../frames/myf_custom.html');
+$('#home').load('frames/myf_home.html');
+$('#menu1').load('frames/myf_turnbuckles.html');
+$('#menu2').load('frames/myf_pondBoatTurnbuckles.html');
+$('#menu3').load('frames/myf_pondBoatFittings.html');
+$('#menu4').load('frames/myf_goosenecks.html');
+$('#menu5').load('frames/myf_fittings.html');
+$('#menu6').load('frames/myf_custom.html');
